98ca82707e3a82b9a369bc9b6f73c153c203ed1b
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-02-20 15:32:36 +0100

    test for identity on github

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = "~> 1.6.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.86.0"
+      version = "~> 3.86"
     }
   }
 }
