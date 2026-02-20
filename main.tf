c5008234de1f7e31c5fcac6738bebcc0bde6aca7
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-02-20 14:18:49 +0100

    test for identity on github

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "demo" {
+  name     = "rg-tf-policy-demo-01"
+  location = "Sweden Central"
+
+  tags = {
+    ManagedBy = "Terraform"
+    Purpose   = "BackendStateProof"
+  }
+}
