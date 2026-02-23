fa6166a523fa54630b186d083662bac98f5344c4
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-02-23 11:01:27 +0100

    changing the .yaml file in order to make jobs more secure

--- backend.tf
+++ backend.tf
@@ -1,10 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate-we-01"
-    storage_account_name = "sttfstatef605f3"
-    container_name       = "tfstate"
-    key                  = "policy/azure-policy-project.tfstate"
-
     use_azuread_auth = true
     use_oidc         = true
   }
