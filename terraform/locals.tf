f993f5e76e5e0e6b0e066ea9f2a874422a561130
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-02-24 17:23:35 +0100

    Shorten policy assignment names and add readable metadata

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  name_prefix         = lower(var.naming_prefix)
-  assignment_prefix   = substr(replace(local.name_prefix, "-", ""), 0, 6)
+  name_prefix          = lower(var.naming_prefix)
+  assignment_prefix    = substr(replace(local.name_prefix, "-", ""), 0, 6)
   allowed_locs_pa_name = "${local.assignment_prefix}-aloc"
 }
