d1f255ba2e6bfbb437ed62d8ea45d1d04152976c
Yaraslau Zhukouski <yaroslav.zhukovski@gmail.com>
2026-02-25 11:14:01 +0100

    cis and mcsb assignment for the management group

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,4 +2,9 @@ locals {
   name_prefix          = lower(var.naming_prefix)
   assignment_prefix    = substr(replace(local.name_prefix, "-", ""), 0, 6)
   allowed_locs_pa_name = "${local.assignment_prefix}-aloc"
+
+  builtin_initiatives = {
+    mcsb = "/providers/Microsoft.Authorization/policySetDefinitions/1f3afdf9-d0c9-4c3d-847f-89da613e70a8"
+    cis  = "/providers/Microsoft.Authorization/policySetDefinitions/612b5213-9160-4969-8578-1518bd2a000c"
+  }
 }
--- terraform/policy_assignments_builtin.tf
+++ terraform/policy_assignments_builtin.tf
@@ -48,3 +48,25 @@ resource "azurerm_management_group_policy_assignment" "require_tag_resource_grou
     }
   })
 }
+
+resource "azurerm_management_group_policy_assignment" "mcsb" {
+
+  name                 = "${local.assignment_prefix}-mcsb"
+  display_name         = "Microsoft Cloud Security Benchmark (MCSB)"
+  description          = "Applies the Microsoft Cloud Security Benchmark (MCSB) policy set."
+  policy_definition_id = local.builtin_initiatives.mcsb
+  management_group_id  = data.azurerm_management_group.target.id
+  enforce              = var.guardrails_enforcement_mode == "Default"
+
+}
+
+resource "azurerm_management_group_policy_assignment" "cis" {
+
+  name                 = "${local.assignment_prefix}-cis"
+  display_name         = "CIS Microsoft Azure Foundations Benchmark"
+  description          = "Applies the CIS Microsoft Azure Foundations Benchmark policy set."
+  policy_definition_id = local.builtin_initiatives.cis
+  management_group_id  = data.azurerm_management_group.target.id
+  enforce              = var.guardrails_enforcement_mode == "Default"
+
+}
